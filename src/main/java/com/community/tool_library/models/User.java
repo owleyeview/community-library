07297a09335b55ea9261e361351109e33547a1b0
owleyeview <rashaanlightpool@gmail.com>
2024-12-10 14:18:52 -0800

    added User and AdminUser models

---
+++ src/main/java/com/community/tool_library/models/User.java
@@ -0,0 +1,53 @@
+package com.community.tool_library.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USER")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    private String email;
+
+    @Column(insertable = false, updatable = false)
+    private String role;
+
+    private LocalDateTime lastLogin;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
